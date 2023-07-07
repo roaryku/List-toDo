59b9bcc63d3e10a89b9c44df51eed1445b41c268
Rodika Aryku <119984393+roaryku@users.noreply.github.com>
2023-07-06 18:14:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const inputField = document.querySelector('.inputField');
 const btn = document.querySelector('.btn');
 const toDoContainer = document.querySelector('.toDoContainer');
 
+inputField.addEventListener('keydown', (e) => {
+    if(e.keyCode === 13){
+        btn.click();
+    }
+})
+
 btn.addEventListener('click', () => {
     const item = document.createElement('li');
     item.innerText = inputField.value;
